f84cf9894011e2d3389cf8179d58bdf73f81c2cd
CMNisal <cmnisal@gmail.com>
2017-01-08 20:25:02 +0530

    undo names

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ function find($chat_id,$find_keyword,$message_id){
                     $button_text = "📦 ";
             }
             $button_text .= $file_exist[$i]['display_name'];
-			$temp_msg.=$button_text;
+			//$temp_msg.=$button_text;
             error_log($button_text);
             $keyboard['keyboard'][$i+1][0] = urlencode($button_text);
         }
